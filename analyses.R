01a01b16ca69e359e245347f8926363243dceada
VitoTito <vitototi1202@gmail.com>
2024-02-04 15:36:40 +0100

    Vito 7

--- analyses.R
+++ analyses.R
@@ -54,7 +54,15 @@ seuil <- 13.8
 variables_a_supprimer <- names(occurrences2)[sapply(occurrences2, function(x) any(x <= seuil))]
 variables_a_supprimer
 
+
+
 # Pas de variables avec une modalité inférieure à 15%
+
+rm(variables_a_supprimer)
+rm(occurrences)
+rm(occurrences2)
+rm(base_factor_repro)
+rm(colonnes_chr)
 
 #### Etape 2 : Etude univariée du lien entre la variable Y et les variables X #### 
 
@@ -164,48 +172,76 @@ setdiff(liste_var_etape_2$Variable_X, liste_var_etape_4$Variable_X)
 
 repro <- repro[, (names(repro) %in% c(liste_var$Variable_X, 'y12_BEA_Repro'))]
 
+rm(liste_var_etape_2, liste_var_etape_4, liste_var, nombre_na_par_variable, seuil, seuil2)
+
+
 #### Etape 5 : Etude des corrélations entre les variables X retenues à p < 0,2 ####
 
-# Récupérer le nom des variables de la table
-toutes_les_variables <- names(repro)
+# Sélectionnez les noms des variables numériques
+numeric_variables <- names(repro)[sapply(repro, is.numeric)]
 
-# Initialiser une liste pour stocker les résultats
-resultats_corr <- list()
+# Sélectionnez les noms des variables catégorielles
+categorical_variables <- names(repro)[sapply(repro, is.factor)]
 
-# Boucle pour calculer les corrélations
-for (variable_x in toutes_les_variables) {
-  for (variable_y in toutes_les_variables) {
-    # Exclure la variable elle-même
-    if (variable_x != variable_y) {
-      # Calculer la corrélation de Pearson pour les variables numériques
-      if (is.numeric(repro[[variable_x]]) && is.numeric(repro[[variable_y]])) {
-        correlation <- cor(repro[[variable_x]], repro[[variable_y]], use = "complete.obs")
-        
-        # Vérifier la p-value
-        if (abs(correlation) > 0.05) {
-          resultats_corr[[paste(variable_x, variable_y, sep="_")]] <- list(Variable_X = variable_x, Variable_Y = variable_y, Correlation = correlation)
-        }
-      }
-      # Test du Chi² pour les variables catégorielles
-      else if (is.factor(repro[[variable_x]]) && is.factor(repro[[variable_y]])) {
-        contingency_table <- table(repro[[variable_x]], repro[[variable_y]])
-        chi_square_test <- chisq.test(contingency_table)
-        
-        # Vérifier la p-value
-        if (chi_square_test$p.value < 0.05) {
-          resultats_corr[[paste(variable_x, variable_y, sep="_")]] <- list(Variable_X = variable_x, Variable_Y = variable_y, PValue = chi_square_test$p.value)
-        }
+# Initialisez un data frame pour stocker les résultats
+correlation_df <- data.frame(
+  Variable1 = character(),
+  Variable2 = character(),
+  Type1 = character(),
+  Type2 = character(),
+  P_Value = numeric(),
+  stringsAsFactors = FALSE
+)
+
+# Fonction pour ajouter les résultats au data frame
+add_to_dataframe <- function(test, var1, var2, type1, type2) {
+  correlation_df <<- rbind(correlation_df, data.frame(Variable1 = var1, Variable2 = var2, Type1 = type1, Type2 = type2, P_Value = test$p.value))
+}
+
+# Boucle pour les tests de corrélation entre variables numériques
+for (var1 in numeric_variables) {
+  for (var2 in numeric_variables) {
+    if (var1 != var2) {
+      correlation_test <- cor.test(repro[[var1]], repro[[var2]])
+      if (correlation_test$p.value < 0.05) {
+        add_to_dataframe(correlation_test, var1, var2, "Numeric", "Numeric")
       }
     }
   }
 }
 
-# Compter les occurrences des variables
-occurrences <- table(unlist(lapply(resultats_corr, function(x) x$Variable_X)))
-sort(occurrences)
+# Boucle pour les tests de corrélation entre variables catégorielles
+for (var1 in categorical_variables) {
+  for (var2 in categorical_variables) {
+    if (var1 != var2) {
+      correlation_test <- chisq.test(table(repro[[var1]], repro[[var2]]))
+      if (correlation_test$p.value < 0.05) {
+        add_to_dataframe(correlation_test, var1, var2, "Categorical", "Categorical")
+      }
+    }
+  }
+}
 
-quantitative_data <- repro[, sapply(repro, is.numeric)]  # Select only numeric columns
-results_acp <- PCA(quantitative_data, scale.unit = TRUE, graph = TRUE)
+# Affichez le data frame avec les résultats
+correlation_df
+sort(table(correlation_df$Variable1))
+
+# Que garder ?
+
+attr(repro$X17x2_INTDOCAST, "label") <- "Intensité douleur castration de 1 à 10"
+attr(repro$X17x1_LIBLACT , "label") <- "Truies bloquées en maternité et libérées en cours de lactation (1 non, 2 oui)"
+attr(repro$T06_VG_ZonDelPasContacSang, "label") <- "Si bâtiment fermé, zone d'élevage physiquement délimitée empêchant tout contact entre les suidés [...]"
+attr(repro$X17x3_CMAMB, "label") <- "Congestions mammaires suite MB ? (1 non, 2 oui)"
+attr(repro$X17x2_PLSECH_rec, "label") <- "Porcelets séchés ? (1 non, 2 oui)"
+attr(repro$X01x1_PPHBTEL, "label") <- "Panneaux photovoltaïques sur bâtiment élevage"
+attr(repro$X17x1_DELRET_rec, "label") <- "Délivres retirées des cases ? (1 non, 2 oui)"
+attr(repro$X17x1_QMALMAT_2, "label") <- "Quantité max distribuée en maternité (kg)"
+attr(repro$X17x1_DELAIS_rec, "label") <- "Délivres laisées dans les cases ? (1 non, 2 oui)"
+attr(repro$X17x2_AGCAST, "label") <- "Âge de castration"
+attr(repro$X17x2_ANALGCAST, "label") <- "Analgésique reçu ? (1 non, 2 oui)"
+attr(repro$X17x3_AGADO_3, "label") <- "Porcelets adoptés à partir de quel âge ?"
+                  
+variables_a_garder <- c('a')
 
 # Nous observons des variables très fortements corrélées entre elles. On ne garde que quelques variables numériques saillantes
 
